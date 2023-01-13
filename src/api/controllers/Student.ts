da1817048ea23d79a8e7aeb45f55a8575807d09f
Erick Rocha <erick2003rocha@gmail.com>
2023-01-12 21:49:17 -0300

    add student methods and routes

--- src/api/controllers/Student.ts
+++ src/api/controllers/Student.ts
@@ -6,13 +6,61 @@ class StudentController {
   static async create(req: Request, res: Response) {
     try {
       const newStudent = await Student.create(req.body);
-      res.json(newStudent);
+      const { id, completeName, email, age } = newStudent;
+      res.json({ id, completeName, email, age });
     } catch (err) {
       if (err instanceof Error.ValidationError) {
         res.status(400).json(err.message);
       } else {
         res.status(400).json(err);
       }
+    }
+  }
+
+  static async index(req: Request, res: Response) {
+    try {
+      const students = await Student.find().select('id completeName email age');
+      return res.json(students);
+    } catch (err) {
+      return res.status(500).json('Something is wrong');
+    }
+  }
+
+  static async show(req: Request, res: Response) {
+    try {
+      const student = await Student.findById(req.params.id);
+      if (!student) return res.status(401).json('Student does not exist');
+      const { id, completeName, email, age } = student;
+      return res.json({ id, completeName, email, age });
+    } catch (err) {
+      return res.status(400).json('Student not found');
+    }
+  }
+
+  static async update(req: Request, res: Response) {
+    try {
+      const student = await Student.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+      });
+      if (!student) return res.status(401).json('Student does not exist');
+      const { id, completeName, email, age } = student;
+      return res.json({ id, completeName, email, age });
+    } catch (err) {
+      if (err instanceof Error.ValidationError) {
+        return res.status(400).json(err.message);
+      } else {
+        return res.status(400).json(err);
+      }
+    }
+  }
+
+  static async delete(req: Request, res: Response) {
+    try {
+      const student = await Student.findByIdAndDelete(req.params.id);
+      if (!student) return res.status(401).json('Student does not exist');
+      return res.json('Student deleted successfully');
+    } catch (err) {
+      return res.status(400).json('Student not found');
     }
   }
 }
--- src/api/routes/student.ts
+++ src/api/routes/student.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import StudentController from '../controllers/Student';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = Router();
 
-router.post('/create', StudentController.create);
+router.get('/', StudentController.index);
+router.post('/', loginRequired, StudentController.create);
+router.get('/:id', StudentController.show);
+router.put('/:id', loginRequired, StudentController.update);
+router.delete('/:id', loginRequired, StudentController.delete);
 
 export default router;
